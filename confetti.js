2db2b1f6c2ddecfe286a75d33f8555ccc8c3c0a3
mominaiftikhar4742 <mominaiftikhar4742@gmail.com>
2025-02-26 23:54:53 +0500

    Delete confetti.js

--- confetti.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    lottie.loadAnimation({
-        container: document.getElementById('animationContainer'),
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: 'https://assets9.lottiefiles.com/packages/lf20_u4yrau.json' // Change this URL for another animation
-    });
-});
